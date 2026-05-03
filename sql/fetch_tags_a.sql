ab739cd156af55dfad0093c5975d5aecffb3e3e3
Cinder <cinder@e621.net>
2026-05-03 07:32:25 -0700

    [SQL] Optimize tag lookups
    Fixes #11
    
    Fixes #10

--- sql/fetch_tags_a.sql
+++ sql/fetch_tags_a.sql
@@ -1,8 +1,36 @@
-SELECT DISTINCT ON (name, post_count) * FROM (
-(SELECT tags.id, tags.name, tags.post_count, tags.category, null AS antecedent_name FROM
-"tags" WHERE (tags.name LIKE $1 ESCAPE E'\\') AND (post_count > 0) ORDER BY post_count desc LIMIT 10)
+(
+    SELECT
+        tags.id,
+        tags.name,
+        tags.post_count,
+        tags.category,
+        NULL AS antecedent_name
+    FROM tags
+    WHERE tags.name LIKE $1 ESCAPE E'\\'
+      AND tags.post_count > 0
+    ORDER BY tags.post_count DESC
+    LIMIT 10
+)
 UNION ALL
-(SELECT tags.id, tags.name, tags.post_count, tags.category, tag_aliases.antecedent_name
-FROM "tag_aliases"
-INNER JOIN tags ON tags.name = tag_aliases.consequent_name
-WHERE (tag_aliases.antecedent_name LIKE $1 ESCAPE E'\\') AND "tag_aliases"."status" IN ('active', 'processing', 'queued') AND (tags.name NOT LIKE $1 ESCAPE E'\\') AND (tag_aliases.post_count > 0) ORDER BY tag_aliases.post_count desc LIMIT 20)) AS unioned_query ORDER BY post_count desc LIMIT 10
+(
+    SELECT id, name, post_count, category, antecedent_name
+    FROM (
+        SELECT DISTINCT ON (tags.name)
+            tags.id,
+            tags.name,
+            tags.post_count,
+            tags.category,
+            tag_aliases.antecedent_name
+        FROM tag_aliases
+        INNER JOIN tags ON tags.name = tag_aliases.consequent_name
+        WHERE tag_aliases.antecedent_name LIKE $1 ESCAPE E'\\'
+          AND tag_aliases.status IN ('active', 'processing', 'queued')
+          AND tag_aliases.post_count > 0
+          AND tags.name NOT LIKE $1 ESCAPE E'\\'
+        ORDER BY tags.name, tag_aliases.post_count DESC
+    ) deduped_aliases
+    ORDER BY post_count DESC
+    LIMIT 10
+)
+ORDER BY post_count DESC
+LIMIT 10
