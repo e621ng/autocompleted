1865c3a4f283b266f8f22454f28a5f0554f857e4
Cinder <cinder@e621.net>
2026-05-03 17:41:39 -0700

    rework

--- sql/fetch_tags_a.sql
+++ sql/fetch_tags_a.sql
@@ -22,19 +22,29 @@ UNION ALL
         antecedent_name,
         length(antecedent_name) AS name_length
     FROM (
-        SELECT DISTINCT ON (tags.name)
-            tags.id,
-            tags.name,
-            tags.post_count,
-            tags.category,
-            tag_aliases.antecedent_name
-        FROM tag_aliases
-        INNER JOIN tags ON tags.name = tag_aliases.consequent_name
-        WHERE tag_aliases.antecedent_name LIKE $1 ESCAPE E'\\'
-          AND tag_aliases.status IN ('active', 'processing', 'queued')
-          AND tag_aliases.post_count > 0
-          AND tags.name NOT LIKE $1 ESCAPE E'\\'
-        ORDER BY tags.name, tag_aliases.post_count DESC
+        SELECT DISTINCT ON (name)
+            id,
+            name,
+            post_count,
+            category,
+            antecedent_name
+        FROM (
+            SELECT
+                tags.id,
+                tags.name,
+                tags.post_count,
+                tags.category,
+                tag_aliases.antecedent_name
+            FROM tag_aliases
+            INNER JOIN tags ON tags.name = tag_aliases.consequent_name
+            WHERE tag_aliases.antecedent_name LIKE $1 ESCAPE E'\\'
+              AND tag_aliases.status IN ('active', 'processing', 'queued')
+              AND tag_aliases.post_count > 0
+              AND tags.name NOT LIKE $1 ESCAPE E'\\'
+            ORDER BY tags.post_count, length(tag_aliases.antecedent_name) DESC
+            LIMIT 50
+        ) pre_limited
+        ORDER BY name, post_count DESC
     ) deduped_aliases
     ORDER BY post_count DESC
     LIMIT 10
