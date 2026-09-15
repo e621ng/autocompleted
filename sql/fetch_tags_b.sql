e631a7bc02d04757d7b8531a7184f7888c9b8d0d
Cinder <cinder@e621.net>
2026-09-15 10:33:15 -0700

    [SQL] Reject non-word input in stage B

--- sql/fetch_tags_b.sql
+++ sql/fetch_tags_b.sql
@@ -1,1 +1,12 @@
-SELECT tags.id, tags.name, tags.post_count, tags.category, null AS antecedent_name FROM "tags" WHERE (tags.name % $1) AND (tags.post_count > 0) ORDER BY trunc(3 * similarity(name, $1)) DESC, post_count DESC, name DESC LIMIT 10
+SELECT
+    tags.id,
+    tags.name,
+    tags.post_count,
+    tags.category,
+    NULL AS antecedent_name
+FROM tags
+WHERE show_trgm($1) <> '{}'
+  AND tags.name % $1
+  AND tags.post_count > 0
+ORDER BY trunc(3 * similarity(name, $1)) DESC, post_count DESC, name DESC
+LIMIT 10;
